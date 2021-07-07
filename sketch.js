5748a8bf087cc6f6c9524e6f3848dfc5ecaf9b61
prachi sharma <67380116+prachi0603@users.noreply.github.com>
2021-07-07 18:34:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function preload()
 	bgImg = loadImage("images/starNight.png");
 	//load animation for fairy here
 	fairyImage = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
-	fairySound = loadSound("sound/joyMusic.mp3")	
+	fairySound = loadSound("sound/JoyMusic.mp3")	
 }
 
 function setup() {
